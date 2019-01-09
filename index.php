fd109ef4a7af813f05d7bdbbb0c909fb98dd9a70
Fernando <fernandosma282@gmail.com>
2019-01-09 10:25:59 -0300

    v 1.0

--- index.php
+++ index.php
@@ -4,12 +4,38 @@
   </head>
   <body>
 
+  <div id = "formularioInical">
+  <input type = "number" id = "questaoInicial" placeholder="Questão inicial"> <input type = "number" id = "questaoFinal" placeholder="Questão final"><br>
+  Limitador de questão ex: 10. <input type = "text" id = "limitadorDeQuestao" value = ". "><br>
+  <textarea id = "texto" placeholder="Digite o texto a ser identificado"></textarea><br>
+  <select id = "tabela">
+    <option value="questoes">Questoes</option>
+    <option value="biologia">Biologia</option>
+    <option value="espanhol">Espanhol</option>
+    <option value="filosofia">Filosofia</option>
+    <option value="fisica">Física</option>
+    <option value="geografia">Geografia</option>
+    <option value="historia">História</option>
+    <option value="ingles">Ingles</option>
+    <option value="matematica">Matemática</option>
+    <option value="portugues">Portugues</option>
+    <option value="quimica">Química</option>
+    <option value="sociologia">Sociologia</option>
+  </select>
+
+  </div>
+
+  <div id = "formulario">
+  </div>
+
     <script src="texto.js"></script>
     <script src="util.js"></script>
     <script>
-      // Transformar as questoes em array
-      questaoInicial = 16;
-      questaoFinal = 20;
+    function iniciar(){
+      texto  = document.getElementById('texto').value;
+      questaoInicial = parseInt(document.getElementById('questaoInicial').value);
+      questaoFinal = parseInt(document.getElementById('questaoFinal').value);     
+      limitadorDeQuestao = document.getElementById('limitadorDeQuestao').value;
       questoes = new Array();
 
       for (j = 0; j <= questaoFinal - questaoInicial; j++) {
@@ -17,8 +43,8 @@
 
         var questao = cortar(
           texto,
-          j + questaoInicial + ". ",
-          j + questaoInicial + 1 + ". "
+          j + questaoInicial + limitadorDeQuestao,
+          j + questaoInicial + 1 + limitadorDeQuestao
         );
         pergunta = cortar(questao, 0, "A) ");
 
@@ -40,16 +66,20 @@
       // criar interface grafica
 
       for(k = 0 ; k < questoes.length ; k++){
-          document.write("<textArea id='id"  + k.toString() +"0'>" + questoes[k][0] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"1'>" + questoes[k][1] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"2'>" + questoes[k][2] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"3'>" + questoes[k][3] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"4'>" + questoes[k][4] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"5'>" + questoes[k][5] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"6'>" + questoes[k][6] + "</textArea>");
-          document.write("<textArea id='id"  + k.toString() +"7'>" + questoes[k][7] + "</textArea>");
-          document.write("<br>")
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x0'>" + questoes[k][0] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x1'>" + questoes[k][1] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x2'>" + questoes[k][2] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x3'>" + questoes[k][3] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x4'>" + questoes[k][4] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x5'>" + questoes[k][5] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x6' maxlength='1' style = 'width: 50px'>" + questoes[k][6] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<textArea id='id"  + k.toString() +"x7'>" + questoes[k][7] + "</textArea>");
+          document.getElementById('formulario').innerHTML += ("<br>")
       }
+      document.getElementById("gerarVariavel").hidden = false;
+      document.getElementById("iniciar").hidden = true;
+      document.getElementById("texto").hidden = true;
+    }
 
       questoesFinalizadas = new Array();
       function finalizar(){
@@ -57,56 +87,62 @@
           for(i = 0 ; i < questoes.length ; i++){
             questoesFinalizadas.push(
                 new Array(
-                    document.getElementById("id" + i.toString() + "0").value,
-                    document.getElementById("id" + i.toString() + "1").value,
-                    document.getElementById("id" + i.toString() + "2").value,
-                    document.getElementById("id" + i.toString() + "3").value,
-                    document.getElementById("id" + i.toString() + "4").value,
-                    document.getElementById("id" + i.toString() + "5").value,
-                    document.getElementById("id" + i.toString() + "6").value,
-                    document.getElementById("id" + i.toString() + "7").value,
+                    document.getElementById("id" + i.toString() + "x0").value,
+                    document.getElementById("id" + i.toString() + "x1").value,
+                    document.getElementById("id" + i.toString() + "x2").value,
+                    document.getElementById("id" + i.toString() + "x3").value,
+                    document.getElementById("id" + i.toString() + "x4").value,
+                    document.getElementById("id" + i.toString() + "x5").value,
+                    document.getElementById("id" + i.toString() + "x6").value,
+                    document.getElementById("id" + i.toString() + "x7").value,
                     )
             )
           }
-          console.log("Finalizado");
           console.log(questoesFinalizadas); 
           // Criando sql
-          var sql = "INSERT INTO `questoes` (`per`, `a`, `b`, `c`, `d`, `e`, `rc`, `img`) VALUES ("
+          var sql = "INSERT INTO `" + document.getElementById("tabela").value +"` (`per`, `a`, `b`, `c`, `d`, `e`, `rc`, `img`) VALUES ("
     
           for(i = 0 ; i < questoesFinalizadas.length ; i++){
               if(i > 0){
                   sql = sql + "("
               }
-              sql = sql + "'" + questoesFinalizadas[i][0] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][1] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][2] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][3] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][4] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][5] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][6] + "',";
-              sql = sql + "'" + questoesFinalizadas[i][7] + "'";
+              sql = sql + "'" + questoesFinalizadas[i][0].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][1].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][2].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][3].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][4].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][5].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][6].trim() + "',";
+              sql = sql + "'" + questoesFinalizadas[i][7].trim() + "'";
               sql = sql + "),"
-
-
           }
-          sql = sql.substr(0,sql.length - 1)
+
+          sql = sql.substr(0,sql.length - 1);
+          // Retirando todos os undefined's
+          strbusca = eval('/'+ "undefined" +'/g');
+          sql = sql.replace(strbusca , ""); 
+          questoesFinalizadas = [];
           console.warn(sql)
           document.getElementById('qualquer').value = sql;
+          document.getElementById("finalizar").hidden = false;
           }
       
       </script>
+
     <?php
     $a = '<script>document.write(sql)</script>';
     ?>
 
-<button type="button" onclick="finalizar()">GERAR VARIAVEL</button>
+    <button type="button" id = "iniciar" onclick="iniciar()">Iniciar</button><br>
+    <button type="button" id = "gerarVariavel" onclick="finalizar()" hidden="hidden" >GERAR VARIAVEL</button>
 
     <form method="POST" action="insere.php">
         <input type="text" hidden="hidden" id = "qualquer" name="teste"/>
         <br>
-        <button type="submit" >Finalizar</button>
+        <button type="submit"  id = "finalizar" hidden="hidden">Finalizar</button>
 
-      </form>
+     </form>
+
  
   </body>
 </html>
